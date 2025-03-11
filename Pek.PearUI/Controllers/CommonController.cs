bcd81160f5145a2306a9d5b5da0c9dade5f398e8
猿人易 <qcjxberin@outlook.com>
2025-03-11 17:56:12 +0800

    新增公共接口和消息数据模型
    在 `CommonController.cs` 中新增 `CommonController` 类，包含 `GetHeadMessage` 和 `GetProfilePicture` 方法，用于获取未读通知和用户头像。添加了获取用户头像和默认头像的辅助方法。
    
    在 `MessageDto.cs` 中新增 `MessageDto` 和 `MessageChild` 类，分别用于表示消息及其子消息的详细信息。

---
+++ Pek.PearUI/Controllers/CommonController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NewLife;
+
+using Pek.Mime;
+using Pek.MVC;
+using Pek.NCube;
+using Pek.NCubeUI;
+using Pek.PearUI.Models;
+using Pek.Swagger;
+
+using XCode.Membership;
+
+namespace Pek.PearUI.Controllers;
+
+/// <summary>
+/// 公共接口
+/// </summary>
+[Produces("application/json")]
+[CustomRoute(ApiVersions.V1)]
+[DHAuthorize]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class CommonController : PekBaseControllerX
+{
+    /// <summary>
+    /// 获取未读通知数量
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("GetHeadMessage")]
+    public IActionResult GetHeadMessage()
+    {
+        var list = new List<MessageDto>();
+        var model = new MessageDto();
+        model.id = 1;
+        model.title = GetResource("通知");
+        model.children = new List<MessageChild>();
+        model.children.Add(new MessageChild
+        {
+            id = 1,
+            avatar = "https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png",
+            title = GetResource("欢迎使用本系统"),
+            context = GetResource("欢迎使用本系统"),
+            form = GetResource("系统"),
+            time = GetResource("刚刚")
+        }); ;
+
+        list.Add(model);
+
+        return Json(list);
+    }
+
+    /// <summary>
+    /// 获取用户头像
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("GetProfilePicture")]
+    public FileResult GetProfilePicture()
+    {
+        var profilePictureContent = GetUserProfilePicture();
+        if (profilePictureContent.IsNullOrWhiteSpace())
+        {
+            return GetDefaultProfilePictureInternal();
+        }
+
+        return File(Convert.FromBase64String(profilePictureContent), MimeTypes.ImagePng);
+    }
+
+    private String GetUserProfilePicture()
+    {
+        var profilePictureContent = String.Empty;
+
+        var user = ManageProvider.User;
+        if (user?.Avatar.IsNullOrWhiteSpace() == false)
+        {
+            var file = user.Avatar.AsFile();
+            if (file.Exists)
+            {
+                profilePictureContent = Convert.ToBase64String(file.ReadBytes());
+            }
+        }
+
+        return profilePictureContent;
+    }
+
+    /// <summary>
+    /// 获取默认头像
+    /// </summary>
+    /// <returns></returns>
+    protected FileResult GetDefaultProfilePictureInternal()
+    {
+        return File(Path.Combine("images", "avatar.jpg"), MimeTypes.ImageJpeg);
+    }
+}
---
+++ Pek.PearUI/Models/MessageDto.cs
@@ -0,0 +1,58 @@
+﻿namespace Pek.PearUI.Models;
+
+/// <summary>
+/// 
+/// </summary>
+public class MessageDto
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public Int32 id { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public String? title { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public IList<MessageChild>? children { get; set; }
+}
+
+/// <summary>
+/// 
+/// </summary>
+public class MessageChild
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public Int32 id { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public String? avatar { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public String? title { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public String? context { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public String? form { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public String? time { get; set; }
+}
